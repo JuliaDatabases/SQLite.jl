f8325928d66c75e6a9d9cc1430ee73cde63be81c
Jacob Quinn <quinn.jacobd@gmail.com>
2013-06-12 00:26:38 -0500

    Pushed some changes to the test.jl file that were specific to my machine

--- test/test.jl
+++ test/test.jl
@@ -1,27 +1,24 @@
-include("C:/Users/karbarcca/Google Drive/Dropbox/Dropbox/GitHub/sqlite.jl/sqlite.jl")
-co = sqlite.connect("test/Chinook_Sqlite.sqlite")
+# include("C:/Users/karbarcca/Google Drive/Dropbox/Dropbox/GitHub/Sqlite.jl/src/Sqlite.jl")
+co = Sqlite.connect(Pkg.dir() * "\\Sqlite\\test\\Chinook_Sqlite.sqlite")
 
-df = sqlite.query("SELECT * FROM Employee;")
-sqlite.createtable(df; name="test")
-sqlite.query("select * from test")
-sqlite.query("drop table test")
+df = Sqlite.query("SELECT * FROM Employee;")
+Sqlite.createtable(df; name="test")
+Sqlite.query("select * from test")
+Sqlite.query("drop table test")
 
-sqlite.query("SELECT * FROM sqlite_master WHERE type='table' ORDER BY name;")
-sqlite.query("SELECT * FROM Album;")
-sqlite.query("SELECT *
+Sqlite.query("SELECT * FROM sqlite_master WHERE type='table' ORDER BY name;")
+Sqlite.query("SELECT * FROM Album;")
+Sqlite.query("SELECT *
 	FROM Artist a 
 	LEFT OUTER JOIN Album b ON b.ArtistId = a.ArtistId 
 	ORDER BY name;")
 
 using DataFrames
 df2 = DataFrame(ones(1000000,5))
-@time sqlite.createtable(df2;name="test2")
-sqlite.query("drop table test2")
+@time Sqlite.createtable(df2;name="test2")
+Sqlite.query("drop table test2")
 
 using DataFrames
-using sqlite
+using Sqlite
 df3 = DataFrame(ones(1000,5))
 sqldf("select * from df3")
-#To Do:
- #finish other functions, csv2table is beefiest (others are just combo functions)
- #create repo/package
