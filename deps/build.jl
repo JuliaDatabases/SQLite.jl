751898b4732c2b935a49ed12b60ad6b21a5024fe
Iain Dunning <iaindunning@gmail.com>
2014-01-18 18:58:58 -0500

    Modernize BinDeps

--- deps/build.jl
+++ deps/build.jl
@@ -4,14 +4,15 @@ using BinDeps
 
 deps = [sqlite3_lib = library_dependency("sqlite3_lib", aliases=["sqlite3","sqlite3-64","libsqlite3"])]
 
-@BinDeps.if_install begin
-
-provides(HomeBrew, {"sqlite" => sqlite3_lib})
-provides(Yum, {"sqlite3-devel" => sqlite3_lib})
-provides(AptGet, {"sqlite3" => sqlite3_lib})
+@osx_only begin
+  using Homebrew
+  provides(Homebrew.HB, {"sqlite" => sqlite3_lib})
+end
+@unix_only
+  provides(Yum, {"sqlite3-devel" => sqlite3_lib})
+  provides(AptGet, {"sqlite3" => sqlite3_lib})
+end
 
 provides(Binaries, {URI("http://drive.google.com/uc?export=download&id=0B6x_qYLl89S0TnAxS1Jtd3g3QkU") => sqlite3_lib}, os = :Windows)
 
 @BinDeps.install
-
-end
