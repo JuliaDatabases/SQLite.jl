690612755644fbec81ebf7b27e3901e8726a57dd
Sean Marshallsay <srm.1708@gmail.com>
2014-10-10 16:34:04 +0100

    Export and test sr_str.

--- test/runtests.jl
+++ test/runtests.jl
@@ -104,7 +104,7 @@ if VERSION > v"0.4.0-"
     @test drop(db,"temp") == EMPTY_RESULTSET
 end
 
-r = query(db, "SELECT  LastName FROM Employee WHERE BirthDate REGEXP '^\\d{4}-08'")
+r = query(db, sr"SELECT  LastName FROM Employee WHERE BirthDate REGEXP '^\d{4}-08'")
 @test r.values[1][1] == "Peacock"
 
 @test size(tables(db)) == (11,1)
